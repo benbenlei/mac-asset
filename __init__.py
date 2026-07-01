0a8df4544f23e8d7f2d38dd36cbe6fd9b01d77b5
Lei Zhang <lei.zhang@live.ca>
2026-07-01 14:03:41 -0400

    add new classify feature

---
+++ __init__.py
@@ -0,0 +1,8 @@
+class Asset():
+    def __init__(self):
+        pass
+    def __abs__(self):
+        pass
+
+    def classify(self):
+        return { "falttype": "mechanical", "severity": "high", "confidence": 8}
